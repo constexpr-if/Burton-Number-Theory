8086858c6f31056c2784b9e05041c7c3b150aa63
constexpr-if <constexpr12@gmail.com>
2024-08-25 12:30:26 +0900

    Change divmod type signature
    from option (nat * nat), to 0 < b -> nat * nat.

--- IntDiv.v
+++ IntDiv.v
@@ -1,22 +1,13 @@
 Require Import Nat Arith Program.
 
-Program Fixpoint divmod (a b: nat) {measure a}: option (nat * nat) :=
-  match b with
-  | 0 => None
-  | _ => match a <? b with
-         | true  => Some (0, a)
-         | false => match divmod (a-b) b with
-                    | None => None
-                    | Some (q, r) => Some (S q, r)
-                    end
-         end
+Program Fixpoint divmod (a b: nat) (H: 0 < b) {measure a}: nat * nat :=
+  match a <? b with
+  | true  => (0, a)
+  | false => let (q, r) := divmod (a-b) b H in (S q, r)
   end.
-Next Obligation.
-  rename Heq_anonymous into le_b_a. rename n into b_not_0.
-  symmetry in le_b_a. apply Nat.ltb_ge in le_b_a.
-  destruct b. destruct b_not_0. reflexivity. clear b_not_0.
-  apply (Nat.add_lt_mono_r _ _ (S b)). rewrite (Nat.sub_add _ _ le_b_a).
-  apply Nat.lt_add_pos_r, Nat.lt_0_succ.
+Next Obligation. clear divmod.
+  rename Heq_anonymous into a_ge_b. symmetry in a_ge_b. apply Nat.ltb_ge in a_ge_b.
+  apply (Nat.sub_lt _ _ a_ge_b H).
 Qed.
 
 Theorem division:
